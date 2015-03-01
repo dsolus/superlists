8846a55e6eb3c2df2f629e16f08c12f65fb37acc
Daniel Solus <solus@Daniel-Soluss-MacBook-Pro.local>
2015-03-01 13:47:21 -0600

    adding another user to FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,10 +38,11 @@ class NewVisitorTest(LiveServerTestCase):
 
 	#So you need to enter a to-do list item right about now
 	    inputbox.send_keys('Buy peacock feathers')
-	    
+
+	#the page now lists "1: Buy peacock feathers"
 	    inputbox.send_keys(Keys.ENTER)
-	    #import time
-	    #time.sleep(10)
+	    edith_list_url = self.browser.current_url
+	    self.assertRegex(edith_list_url, '/list/.+')
 	    self.check_for_row_in_list_table('1: Buy peacock feathers')
 	    
 	#A text box remains inviting further To-Do items naturally
@@ -53,7 +54,32 @@ class NewVisitorTest(LiveServerTestCase):
 	    self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 	    self.check_for_row_in_list_table('1: Buy peacock feathers')
 	
-	#Will the site still remember her list?
-	    self.fail('Finish the test!')
+	#Now a new user, Francis , comes to the site.
 
-	    #the homepage updates and now shows both items
+	#We start a new browser session for Francis.
+
+	    self.browser.quit()
+	    self.browser = webdriver.Firefox()
+
+	#Francis vists the homepage. There is no sign of Ediths list.
+	    self.browser.get(self.live_server_url)
+	    page_text = self.browser.find_element_by_tag_name('body').text
+	    self.assertNotIn('Buy peacock feathers', page_text)
+	    self.assertNotIn('make a fly', page_text)
+
+	#Francis starts a new list.
+	    inputbox = self.browser.find_element_by_id('id_new_item')
+	    inputbox.send_keys('Buy milk')
+	    inputbox.send_keys(Keys.ENTER)
+
+	#Francis gets his own URL
+	    self.assertRegex(francis_list_url, '/lists/.+')
+	    self.assertNotEqual(francis_list_url, edith_list_url)
+
+	#Again there is no trace of edith's list
+	    page_text = self.browser.find_element_by_tag_name('body').text
+	    self.assertNotIn('Buy Peacock feathers', page_text)
+	    self.assertIn('Buy milk', page_text)
+
+	#Satisfied they both go back to sleep.
+
