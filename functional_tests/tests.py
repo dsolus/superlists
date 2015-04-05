796e6bfe280908c0fd105e9ce13c7c677cc872b6
Daniel Solus <solus@Daniel-Soluss-MacBook-Pro.local>
2015-04-04 21:10:07 -0500

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
 #functional_test.py;
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	
 	#setting up browser window in Firefox
 	def setUp(self):
---
+++ lists/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0004_auto_20150302_0145'),
+        ('lists', '0005_remove_list_text'),
+    ]
+
+    operations = [
+    ]
